87c4c2a2da4d0c11d5be41a75eb009b6ba12fe18
Nikhil Limaye <nikhillimaye10@gmail.com>
2020-04-05 14:41:18 -0700

    Harcoded activity objects and IDs

---
+++ Spartan/database/insert_basic_activities.sql
@@ -0,0 +1,13 @@
+
+use cmpe226_spartan;
+
+insert into activity values ("1" , "Yoga");
+insert into activity values ("2" , "Zumba");
+insert into activity values ("3" , "Fitness");
+insert into activity values ("4" , "Basketball");
+insert into activity values ("5" , "Football");
+insert into activity values ("6" , "Volleyball");
+insert into activity values ("7" , "Badminton");
+
+alter table session add session_date date
+
