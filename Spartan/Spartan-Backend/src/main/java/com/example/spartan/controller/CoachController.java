5d776c0dd96cb1ce7ce958f688b0c39d985b8dee
Priya Yadav <07priyayadav@gmail.com>
2020-05-01 00:40:19 -0700

    Create TeamTryOut create session backend

--- Spartan/Spartan-Backend/src/main/java/com/example/spartan/controller/CoachController.java
+++ Spartan/Spartan-Backend/src/main/java/com/example/spartan/controller/CoachController.java
@@ -1,6 +1,7 @@
 package com.example.spartan.controller;
 
 import com.example.spartan.entity.Coach;
+import com.example.spartan.mail.SendMail;
 import com.example.spartan.repository.CoachRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins="*")
 @RestController
@@ -55,4 +57,31 @@ public class CoachController {
 	}
 
 
+
+	@PostMapping("/newTryOutSession")
+	public ResponseEntity<String> createNewSession(@RequestBody Map<String, String> payload) {
+		System.out.println("New try session to be created - \n"+
+				"session name = "+(String)payload.get(payload.keySet().toArray()[1])+
+				"email="+(String)payload.get(payload.keySet().toArray()[4]));
+
+		try {
+			coachRepo.createTryOutSession(payload);
+
+			System.out.println("SUCCESS");
+
+			String receiver = (String)payload.get(payload.keySet().toArray()[4]);
+			if(!receiver.equals("")) {
+				SendMail y = new SendMail();
+				y.sendEmail("You have Created TryOut Session in Spartan Recreation", receiver,
+						"You have Created Session in Spartan Recreation with name "+ (String)payload.get(payload.keySet().toArray()[1])+"." +"\n\n For more details check your dashboard\n\n " +
+								"Thanks and Regards, \n Spartan Recreation Team");
+			}
+
+			return ResponseEntity.ok().body("");
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getCause().toString());
+		}
+	}
 }
